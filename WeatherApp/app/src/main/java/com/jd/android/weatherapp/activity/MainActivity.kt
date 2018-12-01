0c2b63478deb9a7b6805bc5996a855d8469187bc
JD <juandiegogarita@gmail.com>
2018-11-30 23:21:13 -0600

    Starts using anko functions

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.jd.android.weatherapp.R
 import com.jd.android.weatherapp.adapter.ForecastListAdapter
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        val forecastList : RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
     }
