8b23f36523fbf72b9918750bf7f6c49b1a12d46c
JD <juandiegogarita@gmail.com>
2018-12-01 00:13:10 -0600

    Includes mapper class to map the data

---
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/domain/mappers/ForecastDataMapper.kt
@@ -0,0 +1,41 @@
+package com.jd.android.weatherapp.domain.mappers
+
+import com.jd.android.weatherapp.data.Forecast
+import com.jd.android.weatherapp.data.ForecastResult
+import com.jd.android.weatherapp.domain.model.ForecastList
+import java.text.DateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+import com.jd.android.weatherapp.domain.model.Forecast as ModelForecast
+
+class ForecastDataMapper {
+
+    fun convertFromDataModel(forecast: ForecastResult):
+            ForecastList =
+        ForecastList(
+            forecast.city.name,
+            forecast.city.country,
+            convertForecastListToDomain(forecast.list)
+        )
+
+    private fun convertForecastListToDomain(list: List<Forecast>): List<ModelForecast> {
+        return list.mapIndexed { index, forecast ->
+            val dt = Calendar.getInstance().timeInMillis
+
+            TimeUnit.DAYS.toMillis(index.toLong())
+            convertForecastItemToDomain(forecast.copy(dt = dt))
+        }
+    }
+
+    private fun convertForecastItemToDomain(forecast: Forecast): ModelForecast {
+        return ModelForecast(
+            convertDate(forecast.dt), forecast.weather[0].description,
+            forecast.temp.max.toInt(), forecast.temp.min.toInt()
+        )
+    }
+
+    private fun convertDate(date: Long): String {
+        val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+        return df.format(date)
+    }
+}
