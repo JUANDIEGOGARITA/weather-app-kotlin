c3cfcb72abfe88e25a1a58ae0c178d226a689210
JD <juandiegogarita@gmail.com>
2018-11-30 23:08:24 -0600

    Includes the forecast list and makes it available from the activity

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.jd.android.weatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello from Kotlin"
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
     }
 }
