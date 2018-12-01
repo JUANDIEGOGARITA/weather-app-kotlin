06d5e174c7f00035fc274faaed6d49b22ed91be7
JD <juandiegogarita@gmail.com>
2018-12-01 00:12:52 -0600

    Removes unneeded import

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/domain/model/DomainClasses.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/domain/model/DomainClasses.kt
@@ -1,6 +1,5 @@
 package com.jd.android.weatherapp.domain.model
 
-import com.jd.android.weatherapp.data.Forecast
 
 data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
 
