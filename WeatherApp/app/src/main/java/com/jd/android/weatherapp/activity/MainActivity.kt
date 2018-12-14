8bb2548c61c1cb6cbca0c5e915dc256cebf80c16
JD <juandiegogarita@gmail.com>
2018-12-13 23:11:02 -0600

    starts using the synthetic magic instead of the anko function

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
@@ -3,12 +3,11 @@ package com.jd.android.weatherapp.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.jd.android.weatherapp.R
 import com.jd.android.weatherapp.adapter.ForecastListAdapter
 import com.jd.android.weatherapp.domain.commands.RequestForecastCommand
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
@@ -17,8 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList: RecyclerView = find(R.id.forecast_list)
-        forecastList.layoutManager = LinearLayoutManager(this)
+
+        forecastList.layoutManager =
+                LinearLayoutManager(this)
 
         doAsync {
 
