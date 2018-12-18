3e6aefb630debd18aa501943029724453b1ce1d2
JD <juandiegogarita@gmail.com>
2018-12-17 22:04:12 -0600

    includes more convert functions

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/data/db/DbDataMapper.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/data/db/DbDataMapper.kt
@@ -17,4 +17,17 @@ class DbDataMapper{
             with(dayForecast){
                 Forecast(date, description, high, low, iconUrl)
             }
+
+    fun convertFromDomain(forecast: ForecastList) =
+            with(forecast){
+                val daily = dailyForecast.map {
+                    convertDayFromDomain(id, it)
+                }
+                CityForecast(id, city, country, daily)
+            }
+
+    private fun convertDayFromDomain(cityId: Long, forecast: Forecast) =
+            with(forecast){
+                DayForecast(date, description, high, low, iconUrl, cityId)
+            }
 }
