666e8d253ff4848f5f554052829ce9116e126ed9
JD <juandiegogarita@gmail.com>
2018-12-18 15:43:37 -0600

    Includes new extension functions

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/extensions/CollectionExtensions.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/extensions/CollectionExtensions.kt
@@ -1,4 +1,13 @@
 package com.jd.android.weatherapp.extensions
 
 fun <K, V : Any> MutableMap<K, V?>.toVarargArray():
-        Array<out Pair<K, V>> = map {Pair(it.key, it.value!!)}.toTypedArray()
+        Array<out Pair<K, V>> = map { Pair(it.key, it.value!!) }.toTypedArray()
+
+inline fun <T, R : Any>
+        Iterable<T>.firstResult(predicate: (T) -> R?): R {
+    for (element in this) {
+        val result = predicate(element)
+        if (result != null) return result
+    }
+    throw NoSuchElementException("No element matching predicate was found.")
+}
