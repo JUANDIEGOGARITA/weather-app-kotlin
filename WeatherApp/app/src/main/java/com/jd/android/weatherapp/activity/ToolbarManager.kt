a2d0f1373559ec779312ef6ca0abcf01fe1bca11
JD <juandiegogarita@gmail.com>
2019-01-08 21:13:51 -0600

    includes new class to manage the toolbar in the main activity

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/ToolbarManager.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/ToolbarManager.kt
@@ -1,4 +1,50 @@
 package com.jd.android.weatherapp.activity
 
+import android.support.v7.graphics.drawable.DrawerArrowDrawable
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import com.jd.android.weatherapp.R
+import com.jd.android.weatherapp.app.App
+import com.jd.android.weatherapp.extensions.slideEnter
+import com.jd.android.weatherapp.extensions.slideExit
+import org.jetbrains.anko.toast
+
 interface ToolbarManager {
+    val toolbar: Toolbar
+
+    var toolbarTitle: String
+        get() = toolbar.title.toString()
+        set(title) {
+            toolbar.title = title
+        }
+
+    fun initToolbar() {
+        toolbar.inflateMenu(R.menu.menu_main)
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_settings ->
+                    App.instance.toast("Settings")
+                else -> App.instance.toast("Unknown option")
+            }
+            true
+        }
+    }
+
+    fun enableHomeAsUp(up: () -> Unit) {
+        toolbar.navigationIcon = createUpDrawable()
+        toolbar.setNavigationOnClickListener { up() }
+    }
+
+    private fun createUpDrawable() =
+        DrawerArrowDrawable(toolbar.context).apply { progress = 1f }
+
+    fun attachToScroll(recyclerView: RecyclerView) {
+        recyclerView.addOnScrollListener(object :
+            RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0) toolbar.slideExit() else
+                    toolbar.slideEnter()
+            }
+        })
+    }
 }
--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/extensions/ViewExtensions.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/extensions/ViewExtensions.kt
@@ -1,7 +1,16 @@
 package com.jd.android.weatherapp.extensions
 
+import android.view.View
 import android.widget.TextView
 
 var TextView.textColor: Int
     get() = currentTextColor
     set(color) = setTextColor(color)
+
+fun View.slideExit() {
+    if (translationY == 0f) animate().translationY(-height.toFloat())
+}
+
+fun View.slideEnter() {
+    if (translationY < 0f) animate().translationY(0f)
+}
