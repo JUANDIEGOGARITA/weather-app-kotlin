b4a5b5e44d57eb9998ebabe39cc62c5a3a7a158e
JD <juandiegogarita@gmail.com>
2019-01-08 21:14:13 -0600

    updates activity to implement the custom toolbar programatically

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
@@ -3,19 +3,27 @@ package com.jd.android.weatherapp.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import com.jd.android.weatherapp.R
 import com.jd.android.weatherapp.adapter.ForecastListAdapter
 import com.jd.android.weatherapp.domain.commands.RequestForecastCommand
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ToolbarManager {
+
+    override val toolbar by lazy {
+        find<Toolbar>(R.id.toolbar)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
 
         forecastList.layoutManager = LinearLayoutManager(this)
 
