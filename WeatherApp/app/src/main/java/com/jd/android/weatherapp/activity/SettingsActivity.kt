94bdcab30694f2f352f50fe2ab6def68f53ef38d
JD <juandiegogarita@gmail.com>
2019-01-12 17:51:15 -0600

    includes settings activity

---
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.jd.android.weatherapp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
