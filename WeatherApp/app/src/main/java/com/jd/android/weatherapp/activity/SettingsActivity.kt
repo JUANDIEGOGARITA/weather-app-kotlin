c69c413cf390f83fae00f3ae70cd7c3ed163a66e
JD <juandiegogarita@gmail.com>
2019-01-13 20:39:30 -0600

    includes basic logic in settings activity

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/SettingsActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/SettingsActivity.kt
@@ -1,12 +1,27 @@
 package com.jd.android.weatherapp.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.jd.android.weatherapp.R
+import kotlinx.android.synthetic.main.toolbar.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?) =
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> false
+        }
 }
