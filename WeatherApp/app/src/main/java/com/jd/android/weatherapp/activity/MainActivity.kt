d59f8b8ba753a4034f05bf622eee15be2772e660
JD <juandiegogarita@gmail.com>
2019-01-13 21:06:11 -0600

    updates main activity to start receiving a zipcode from the settings activity

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar
 import com.jd.android.weatherapp.R
 import com.jd.android.weatherapp.adapter.ForecastListAdapter
 import com.jd.android.weatherapp.domain.commands.RequestForecastCommand
+import com.jd.android.weatherapp.extensions.DelegatesExt
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
@@ -19,16 +20,25 @@ class MainActivity : AppCompatActivity(), ToolbarManager {
         find<Toolbar>(R.id.toolbar)
     }
 
+    private val zipCode: Long by
+        DelegatesExt.longPreference(this, SettingsActivity.ZIP_CODE, SettingsActivity.DEFAULT_ZIP)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initToolbar()
-
         forecastList.layoutManager = LinearLayoutManager(this)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        loadForecast()
+    }
+
+    private fun loadForecast() =
         doAsync {
-            val result = RequestForecastCommand(94043).execute()
+            val result = RequestForecastCommand(zipCode).execute()
             uiThread {
                 val adapter = ForecastListAdapter(result) {
                     startActivity<DetailActivity>(
@@ -37,8 +47,7 @@ class MainActivity : AppCompatActivity(), ToolbarManager {
                     )
                 }
                 forecastList.adapter = adapter
-                title = "${result.city} (${result.country})"
+                toolbarTitle = "${result.city} (${result.country})"
             }
         }
-    }
 }
