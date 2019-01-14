ac87ab82eb6a9b0dce9ea53ac7b7521ad1a1e83a
JD <juandiegogarita@gmail.com>
2019-01-13 20:57:31 -0600

    updates settings activity to implement the new delegate extension functions

--- WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/SettingsActivity.kt
+++ WeatherApp/app/src/main/java/com/jd/android/weatherapp/activity/SettingsActivity.kt
@@ -4,9 +4,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import com.jd.android.weatherapp.R
+import com.jd.android.weatherapp.extensions.DelegatesExt
+import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+        const val ZIP_CODE = "zipCode"
+        const val DEFAULT_ZIP = 94049L
+    }
+
+    private var zipCode: Long
+            by DelegatesExt.longPreference(this, ZIP_CODE, DEFAULT_ZIP)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +24,8 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        cityCode.setText(zipCode.toString())
     }
 
     override fun onOptionsItemSelected(item: MenuItem?) =
@@ -24,4 +36,9 @@ class SettingsActivity : AppCompatActivity() {
             }
             else -> false
         }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        zipCode = cityCode.text.toString().toLong()
+    }
 }
